4091cae5817e8d2a5467c396b2adadc08811ea3f
Noa-Fish <noa.watel@gmail.com>
2024-11-13 13:57:03 +0100

    Upgrade

--- Ennemi.java
+++ Ennemi.java
@@ -3,8 +3,4 @@ public class Ennemi extends Personnage {
     public Ennemi(String nom, int pv, int force, int defense, int coordonnee, int distanceAttaque){
             super(nom, pv, force, defense, coordonnee, distanceAttaque);
     }
-    
-    public void capaciteSpeciale() {
-        System.out.println("cameamea");
-    }
 }
--- Personnage.java
+++ Personnage.java
@@ -1,38 +1,73 @@
-public abstract class Personnage {
+public class Personnage {
+    private String nom;
+    private int pv;
+    private int force;
+    private int defense;
+    private int coordonnee;
+    private int distanceAttaque;
 
-        public String nom;
-        public int pv;
-        public int force;
-        public int defense;
-        public int coordonnee;
-        public int distanceAttaque;
-    
-        public Personnage(String nom, int pv, int force, int defense, int coordonnee, int distanceAttaque) {
-            this.nom = nom;
-            this.pv = pv;
-            this.force = force;
-            this.defense = defense;
-            this.coordonnee = coordonnee;
-            this.distanceAttaque = distanceAttaque;
-        }
-    
-        
-        public abstract void capaciteSpeciale();
-    
-        public void avance() {
-            this.coordonnee ++;
-        }
-    
-        public void recul() {
-            this.coordonnee --;
-        }
-    
-        public int getPv() {
-            return this.pv;
-        }
-    
-        public void setPv(int pv) {
-            this.pv = pv;
-        }
-    
+    public Personnage(String nom, int pv, int force, int defense, int coordonnee, int distanceAttaque) {
+        this.nom = nom;
+        this.pv = pv;
+        this.force = force;
+        this.defense = defense;
+        this.coordonnee = coordonnee;
+        this.distanceAttaque = distanceAttaque;
+    }
+
+    public void avance() {
+        this.coordonnee ++;
+    }
+
+    public void recul() {
+        this.coordonnee --;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public int getPv() {
+        return pv;
+    }
+
+    public void setPv(int pv) {
+        this.pv = pv;
+    }
+
+    public int getForce() {
+        return force;
+    }
+
+    public void setForce(int force) {
+        this.force = force;
+    }
+
+    public int getDefense() {
+        return defense;
+    }
+
+    public void setDefense(int defense) {
+        this.defense = defense;
+    }
+
+    public int getCoordonnee() {
+        return coordonnee;
+    }
+
+    public void setCoordonnee(int coordonnee) {
+        this.coordonnee = coordonnee;
+    }
+
+    public int getDistanceAttaque() {
+        return distanceAttaque;
+    }
+
+    public void setDistanceAttaque(int distanceAttaque) {
+        this.distanceAttaque = distanceAttaque;
+    }
 }

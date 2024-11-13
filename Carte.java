8ede2cd2ac39129b5f86ed23141ed7cbac92d0fe
nousseiba7 <nousseiba.tazani@gmail.com>
2024-11-13 10:29:36 +0100

    feat: Carte.java Class

---
+++ Carte.java
@@ -0,0 +1,15 @@
+public class Carte {
+    public String lieu;
+    public String nom;
+    public int debut;
+    public int fin;
+    public int etape;
+
+    public Carte(String lieu, String nom, int debut, int fin, int etape) {
+        this.lieu = lieu;
+        this.nom = nom;
+        this.debut = debut;
+        this.fin = fin;
+        this.etape = etape;
+    }
+}

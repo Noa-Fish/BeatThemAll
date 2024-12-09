e014bfac1d74ad16e1d17c7addf68f0ba666faf0
Noa Watel <noa.watel@gmail.com>
2024-12-09 22:34:49 +0100

    finalisation

--- src/main/java/com/beatthemall/utils/LoggerManager.java
+++ src/main/java/com/beatthemall/utils/LoggerManager.java
@@ -1,11 +1,13 @@
 package com.beatthemall.utils;
 
 import java.util.logging.ConsoleHandler;
+import java.util.logging.FileHandler;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.logging.SimpleFormatter;
 import java.util.logging.Handler;
 import java.util.logging.LogRecord;
+import java.io.IOException;
 
 public class LoggerManager {
 
@@ -23,6 +25,7 @@ public class LoggerManager {
         Logger logger = Logger.getLogger(name);
 
         if (logger.getHandlers().length == 0) {
+            // Console handler
             Handler consoleHandler = new ConsoleHandler();
             consoleHandler.setFormatter(new SimpleFormatter() {
                 @Override
@@ -31,10 +34,24 @@ public class LoggerManager {
                 }
             });
 
+            // File handler
+            Handler fileHandler = null;
+            try {
+                fileHandler = new FileHandler("game_log.log", true);  // true to append to the file
+                fileHandler.setFormatter(new SimpleFormatter());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            // Set logger configurations
             logger.setUseParentHandlers(false);
             logger.addHandler(consoleHandler);
+            if (fileHandler != null) {
+                logger.addHandler(fileHandler);
+            }
             logger.setLevel(Level.INFO);
         }
+
         return logger;
     }
 }
